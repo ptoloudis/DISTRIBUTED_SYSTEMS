cf3628a5f71a5eb1d3e5c271a5231a8154cfb24e
panagiotis toloudis <57294317+ptoloudis@users.noreply.github.com>
2022-03-15 19:42:24 +0200

    Add files via upload

--- buffer_cl.py
+++ buffer_cl.py
@@ -1,40 +1,49 @@
-import threading
-
+import numpy as np
 
 class serveries:
     def __init__(self):
         self.id[3][3] = []
         self.index = 0
-    def add_id(self, id,ip):
+    def add_id(self, id,ip,port):
         self.id[self.index][0] = id
         self.id[self.index][1] = ip
         self.id[self.index][2] = 10
         self.index += 1
     def get_id(self,id):
         for i in range(0,self.index):
-            if self.id[i][0] == id and self.id[i][2] > 0:
+            if self.id[i][0] == id and self.id[i][3] > 0:
+                return self.id[i][1]
+        return 0;
+    def get_ip(self,id):
+        for i in range(0,self.index):
+            if self.id[i][0] == id:
                 return self.id[i][1]
         return 0;
     def down_id(self,id):
         for i in range(0,self.index):
             if self.id[i][0] == id:
-                self.id[i][2] -= 1
+                self.id[i][3] -= 1
                 return
         return;
 
+def hash(mes):
+    hash = 0
+    for i in  range(len(mes)):
+        data =  ord(mes[i])
+        hash += np.floor(np.log2(data)/np.log2(2)) + 1
+    return(int (hash))
         
-
 class messeges:
     def __init___(self, destination:str, messege:bytes, messege_length:int):
         self.destination = destination
         self.messege = messege
+        self.checksum = hash(messege)
         self.messege_length = messege_length
         self.id = id.get_id()
 
     def return_id(self):
         return self.id
     
-
 class buffer_send:
     def __init__(self,size_max):
         self.max = size_max
@@ -73,3 +82,9 @@ class buffer_re:
                 if(self.data[i].id == id):
                     self.cur -= 1
                     return self.data[i]
+
+    def find(self, mes: str):
+        for i in range(self.cur):
+            if(self.data[i].messege == str):
+                return 0
+        return 1
--- client.py
+++ client.py
@@ -1,22 +1,24 @@
+from ast import While
+from email import message
 import socket
-import select
 from time import sleep
 from random import uniform
 import threading
 from buffer_cl import *
 
-
+PORT_MESS = 5010
 MCAST_PORT = 5006
 MCAST_GRP = '224.1.1.1'
 Ping_pong_port = 5007
+Ping_pong_port2 = 5008
 IP_server = "";
 MULTICAST_TTL = 2
+TTL = 5
 UDP_PORT = 5005
-find = False
 Blabi = False
-tmp = serveries()
+servs = serveries()
 
-def multicast_send(sock, serve):
+def multicast_send(sock, serve:str):
     for i in range(0, 3):
         print("Try to find the server")
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
@@ -25,7 +27,7 @@ def multicast_send(sock, serve):
         UDP_IP =  sock.getsockname()[0]
         sock2 = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
         sock2.bind((UDP_IP, UDP_PORT))
-        sock2.settimeout(uniform(1,4))
+        sock2.settimeout(uniform(3,6))
         try:
             dat, addr = sock2.recvfrom(50) # buffer size is 1024 bytes 
             IP_server = addr[0]
@@ -37,13 +39,11 @@ def multicast_send(sock, serve):
     if find == False:
         print("Server not found")
         exit()
-    i = dat.find(b'_')
-    data = dat[:i].decode("utf-8")
-    z = int(data)+i+1
-    dat2 = dat[i+1:z]
-    if dat2 == b'YES':
+    print(dat[4])
+    data = dat[4] 
+    if data == 49 :
         print("Server found")
-        tmp.add_id(serve, addr[0])
+        servs.add_id(serve, addr[0], addr[1])
         return 1;
     else :
         print("Server not found")
@@ -51,26 +51,30 @@ def multicast_send(sock, serve):
 
 def ping_pong():
     while True:
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-        sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, MULTICAST_TTL)
-        sock.sendto("Pong".encode(), (IP_server, Ping_pong_port))
-        UDP_IP =  sock.getsockname()[0]
-        sock2 = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-        sock2.bind((UDP_IP, Ping_pong_port))
-        sock2.settimeout(uniform(1,5))
-        try:
-            dat, addr = sock2.recvfrom(50) # buffer size is 1024 bytes 
-            find = True
-            break  
-        except socket.timeout:
-            print("Write timeout on socket")
-            continue
-    if find == False:
-        Blabi = True
-        print("Server not found")
-        exit()
-    else:
-        sleep(30)#5 seconds
+        find = False
+        for i in range(0, 3):
+            sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+            sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, MULTICAST_TTL)
+            sock.sendto("Pong".encode(), (IP_server, Ping_pong_port))
+            UDP_IP =  sock.getsockname()[0]
+            sock2 = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+            sock2.bind((UDP_IP, Ping_pong_port2))
+            sock2.settimeout(uniform(1,4))
+            try:
+                dat, addr = sock2.recvfrom(50) # buffer size is 1024 bytes 
+                print("UP")
+                IP_server = addr[0]
+                find = True
+                break   
+            except socket.timeout:
+                continue
+        
+        if find == False:
+            Blabi = True
+            print("Server Down")
+            exit()
+        else:
+            sleep(5)#5 seconds
 
 def send(self,svcid, reqbuf, reqlen, resbuf, reslen):
     tmp = messeges(svcid, reqbuf, reqlen)
@@ -83,20 +87,54 @@ def send(self,svcid, reqbuf, reqlen, resbuf, reslen):
 def send_messeger():
     while True:
         tmp = send_buffer.get()
-        #unpack(tmp.messege)
-        #TODO: send messege
-        #
-        
-        #TODO: receive messege
-        #
-        #pack the messege
-        re_buffer.add(tmp)
+        message_send = tmp.messege_length +  "_1_"+ tmp.checksum +tmp.return_id()+"_"+tmp.messege 
+        if (servs.get_id(tmp.destination) == 0 ):
+            if(multicast_send(tmp.destination) == 0):
+                print("Server not found")
+                return -1;
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.setsockopt(socket.IPPROTO_IP, socket.IP_TTL, TTL)
+        for i in range (0, 5):
+            sock.settimeout(uniform(3,6))
+            try:
+                sock.sendto(message_send.encode(), (servs.get_ip(),PORT_MESS))
+                if (sock.recv(7) == "1_1_1"):
+                    break
+                else :
+                    continue
+            except socket.timeout:
+                continue
+        resive = sock.recv(70)
+        i = resive.find(b'_')
+        data =resive[:i].decode("utf-8")
+        z = int(data)
+        dat2 = resive[i+3:].find(b'_')
+        check = int(resive[i+3:i+3+dat2].decode("utf-8"))
+        mess = resive[i+3+dat2+3:].decode("utf-8")
+        if (check == hash(mess)):
+            sock.sendto("1_1_1".encode(), (servs.get_ip(),PORT_MESS))
+            if re_buffer.find(mess):
+                re_buffer.add(mess) 
+        else:
+            sock.sendto("1_1_0".encode(), (servs.get_ip(),PORT_MESS))
 
-
+ 
 ## MAIN ##
+
 send_buffer = buffer_send(10)
 re_buffer = buffer_re(10)
 id()
 transport = False
 x = threading.Thread(send_messeger)
+z = threading.Thread(ping_pong)
 x.start()
+z.start()
+while True:
+    if (Blabi):
+        print("Fail to connect to server")
+        print("Server Down")
+        x.kill()
+        z.kill()
+        exit() 
+    
+#TODO App in client
